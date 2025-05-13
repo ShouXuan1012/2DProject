70de52bd1a243f667b12284fe82d1239402a98ce
ShouXuan1012 <cuishouxuan@gmail.com>
2025-05-13 20:37:32 +0900

    Update CharacterManager.cs

--- Assets/Script/Managers/CharacterManager.cs
+++ Assets/Script/Managers/CharacterManager.cs
@@ -24,11 +24,21 @@ public class CharacterManager : MonoBehaviour
             return;
         Vector2 pos = spawnedCharacters[currentIndex].transform.position;
 
+        // 1. 현재 위치 기억
+        Vector3 currentPos = spawnedCharacters[currentIndex].transform.position;
+
+        // 2. 현재 캐릭터 비활성화
         spawnedCharacters[currentIndex].SetActive(false);
         spawnedCharacters[index].transform.position = pos;
+
+        // 3. 새 캐릭터 위치 설정 + 활성화
+        spawnedCharacters[index].transform.position = currentPos;
         spawnedCharacters[index].SetActive(true);
+
+        // 4. 인덱스 갱신
         currentIndex = index;
     }
+
 
     public GameObject CurrentCharacter => spawnedCharacters.Count > 0 ? spawnedCharacters[currentIndex] : null;
 }
